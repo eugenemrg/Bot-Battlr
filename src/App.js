2ce2467bba64edad57b0b93e91743289f0ecc3f0
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-30 08:54:25 +0300

    Updated App component, added fetch functionality, added missing component imports, passed fetched bots array as prop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import YourBotArmy from './components/YourBotArmy'
+import BotCollection from './components/BotCollection';
 
 function App() {
+
+  const [allBots, setAllBots] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/bots')
+    .then(r => r.json())
+    .then(data => setAllBots(data))
+  }, [])
 
   return (
     <div className="App">
