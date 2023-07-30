75246d9664351fe1a298551498cc0107ce0be63a
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-30 14:27:59 +0300

    Updated Bot.js, added icons, updated styling for Bot component

--- src/components/Bot.js
+++ src/components/Bot.js
@@ -1,20 +1,48 @@
 import React from 'react'
 
-function Bot({properties}){
+function Bot({ properties }) {
 
-    const {name, health, damage, armor, bot_class, catchphrase, avatar_url} = properties
+    const { name, health, damage, armor, bot_class, catchphrase, avatar_url } = properties
+
+    function getBotClassIcon(botClass) {
+        let icon
+        switch (botClass) {
+            case 'Support':
+                icon = <i className="fa-regular fa-life-ring"></i>
+                break;
+            case 'Medic':
+                icon =  <i className="fa-solid fa-truck-medical"></i>
+                break;
+            case 'Witch':
+                icon = <i className="fa-solid fa-hat-wizard"></i>
+                break;
+            case 'Defender':
+                icon = <i className="fa-solid fa-shield-halved"></i>
+                break;
+            case 'Assault':
+                icon = <i className="fa-solid fa-jet-fighter"></i>
+                break;
+            case 'Captain':
+                icon = <i className="fa-solid fa-medal"></i>
+                break;
+            default:
+                icon = ''
+                break;
+        }
+        return icon
+    }
 
     return (
         <div className="card">
             <img className="avatar" src={avatar_url} alt={name} />
             <div className="description">
-                <p className="identification">{name}</p>
+                <p className="identification">{name}{getBotClassIcon(bot_class)}</p>
                 <p className="catchphrase">{catchphrase}</p>
             </div>
             <div className="stats">
-                <p className="stat">{health}</p>
-                <p className="stat">{damage}</p>
-                <p className="stat">{armor}</p>
+                <p className="stat"><i className="fa-solid fa-heart-pulse"></i>{health}</p>
+                <p className="stat"><i className="fa-solid fa-bolt"></i>{damage}</p>
+                <p className="stat"><i className="fa-solid fa-shield-halved"></i>{armor}</p>
             </div>
         </div>
     )
