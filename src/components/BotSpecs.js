124f83a79cb596221d2ad09bbfe0ff21a7aa5576
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-30 22:15:22 +0300

    Updated BotSpecs component content and styling

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import './BotSpecs.css'
 
-function BotSpecs() {
+function BotSpecs({bot}) {
+
+    const { id, name, health, damage, armor, bot_class, catchphrase, avatar_url } = bot
+
     return (
-        <p>Bot Specs</p>
+        <div className="specs-container">
+            <div className="specs">
+                <div className="current-avatar">
+                    <img className="avatar" src={avatar_url} alt="robot" />
+                </div>
+                <div className="current-details">
+                    <p className="current-name">Name: {name}</p>
+                    <p className="current-phrase-header">Catchphrase</p>
+                    <p className="current-phrase">{catchphrase}</p>
+                    <p className="current-class">Class: {bot_class} <i className="class-icon fa-solid fa-shield-halved"></i></p>
+                    <div className="current-stats">
+                        <p className="current-stat"><i className="health-icon fa-solid fa-heart-pulse"></i>{health}</p>
+                        <p className="current-stat"><i className="damage-icon fa-solid fa-bolt"></i>{damage}</p>
+                        <p className="current-stat"><i className="armor-icon fa-solid fa-shield-halved"></i>{armor}</p>
+                    </div>
+                    <button>Go Back</button>
+                    <button>Enlist</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
