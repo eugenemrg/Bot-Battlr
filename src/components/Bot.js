cb5cce27e519f5ce5d9bcc10ea83c5fedc45220d
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-07-30 08:56:22 +0300

    Updated Bot component, added styling and passed props to values needed

--- src/components/Bot.js
+++ src/components/Bot.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
-function Bot(){
+function Bot({properties}){
+
+    const {name, health, damage, armor, bot_class, catchphrase, avatar_url} = properties
+
     return (
-        <div>Bot</div>
+        <div className="card">
+            <img className="avatar" src={avatar_url} alt={name} />
+            <div className="description">
+                <p className="identification">{name}</p>
+                <p className="catchphrase">{catchphrase}</p>
+            </div>
+            <div className="stats">
+                <p className="stat">{health}</p>
+                <p className="stat">{damage}</p>
+                <p className="stat">{armor}</p>
+            </div>
+        </div>
     )
 }
 
